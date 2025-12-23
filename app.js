8b7bd4b28e9d06816bf3546846dad39b90921dee
Ibrokhim Khamraev <i.khamraev0095@gmail.com>
2025-12-23 16:24:16 +0500

    December23 16:24

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-   origin: "http://localhost:5173",
-   credentials: false,
+   origin: "https://ijara.netlify.app/rent/customers",
+   credentials: true,
    exposedHeaders: ["set-cookie"]
 }))
 
